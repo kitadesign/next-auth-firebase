c761fcea08d17e3b546629a71d11e862d800c25e
kitazawa <kitadesign@gmail.com>
2022-11-08 15:14:29 +0900

    コメント追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 // 'use client'
 
+// NOTE: next/navigationに変わっている
 // import { useRouter } from 'next/navigation'
 import Link from 'next/link'
-//
+
 export default function Page() {
   // const router = useRouter()
   return (
     <div>
       <h1>Hello, Next.js!</h1>
+      {/* NOTE: routerを使うとclient componentにする必要がある */}
       {/* <div>
         <button type="button" onClick={() => router.push('/next')}>button</button>
       </div> */}
