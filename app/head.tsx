a50d7cebbf5557ef0493c825c4a26549af7fa6a2
kitazawa <kitadesign@gmail.com>
2022-11-08 15:05:41 +0900

    server componentsとmeta設定

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,12 @@ export const defaultTitle = 'テストサイト'
 export default function Head () {
   return (
     <>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="ja_JP"/>
       <title>{defaultTitle}</title>
     </>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,19 @@
+// 'use client'
 
+// import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+//
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  // const router = useRouter()
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      {/* <div>
+        <button type="button" onClick={() => router.push('/next')}>button</button>
+      </div> */}
+      <div>
+        <Link href="/next">link</Link>
+      </div>
+    </div>
+  )
 }
